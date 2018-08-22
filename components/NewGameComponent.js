4577c4eebfac4c557adb8cce5f4e57eb555015f9
anhvmz <anhvmz@gmail.com>
2018-08-22 16:53:02 +0700

    adsdad

--- components/NewGameComponent.js
+++ components/NewGameComponent.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
-import { Text, View, Image, StyleSheet } from 'react-native';
+import { Text, View, Image, StyleSheet, TextInput, KeyboardAvoidingView, TouchableWithoutFeedback } from 'react-native';
 import Button from 'react-native-button'
 import BottomView from '../commons/BottomView';
+import dismissKeyboard from 'react-native/Libraries/Utilities/dismissKeyboard';
 
 export default class NewGameComponent extends Component {
     static navigationOptions = ({ navigation }) => {
@@ -18,18 +19,43 @@ export default class NewGameComponent extends Component {
         return { headerTitle, headerStyle, headerTitleStyle }
     }
 
-    state = {}
-    _onCreate() {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            newName: ``,
+            newNumberPlayers: ``,
+        };
     }
+
+    _onCreate() {
+        this.props.navigation.goBack()
+    }
+
     render() {
         return (
             <View style={styles.root}>
-                <View style={{
-                    flex: 90
-                }}>
-                    
-                </View>
+                <TouchableWithoutFeedback style={{ flex: 1 }} onPress={dismissKeyboard}>
+                    <View style={{
+                        flex: 90,
+                        justifyContent: 'center',
+                        backgroundColor: "white",
+                    }}>
+                        <TextInput
+                            style={styles.inputText}
+                            onChangeText={(newName) => this.setState({ newName })}
+                            value={this.state.newName}
+                            placeholder={`Input name of game`}
+                        />
+
+                        <TextInput
+                            style={styles.inputText}
+                            onChangeText={(newNumberPlayers) => this.setState({ newNumberPlayers })}
+                            value={this.state.newNumberPlayers}
+                            placeholder={`Input number of players`}
+                        />
+
+                    </View>
+                </TouchableWithoutFeedback>
                 <BottomView onPress={this._onCreate.bind(this)} text={`Create`} />
             </View>
 
@@ -38,10 +64,19 @@ export default class NewGameComponent extends Component {
 }
 
 const styles = StyleSheet.create({
-    
-    root: { 
+
+    root: {
         flex: 1,
         backgroundColor: 'white'
+    },
+
+    inputText: {
+        margin: 10,
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        borderRadius: 4,
+        backgroundColor:'rgba(255,255,0,0.2)',
     }
-    
+
 });
